0eededda8553b48ecd9bbf3f444c27d5f433c618
Patrick Furman <GAR_pnfurman@seattleschools.org>
2019-01-16 10:30:28 -0800

    view food details should display food description now

--- app/src/main/java/com/homcooked/homecooked/view_food_details.java
+++ app/src/main/java/com/homcooked/homecooked/view_food_details.java
@@ -1,7 +1,9 @@
 package com.homcooked.homecooked;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class view_food_details extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class view_food_details extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_food_details);
+        Intent intent = getIntent();
+        String descriptionInfo = intent.getExtras().toString();
+        TextView description = findViewById(R.id.post_description);
+        description.setText(descriptionInfo);
     }
 }
